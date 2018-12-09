9269d609b347094df7ebf2bcb23790fe96484f63
emily seieroe <e.e.seieroe@gmail.com>
2018-12-09 22:23:15 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,27 +2,65 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends => [
+        :friend1 => { 
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        :friend2 => { 
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }  
+        
+        
+        ]
    }, 
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        :friend1 => { 
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        :friend2 => { 
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }  
+      ]
    }
   }
 
