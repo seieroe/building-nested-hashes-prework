751440a775b58d4e23f3cd8d6b792ff0927bac03
emily seieroe <e.e.seieroe@gmail.com>
2018-12-09 22:22:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,21 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+    :montague => {} ,  
+     
+     
+    :capulet => {}
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    
+   }
+
 end
+
+
